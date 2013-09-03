b236012ef4b50867916bbd3a01e195722c9ad469
NachoSoto <ignaciosoto90@gmail.com>
2013-09-03 22:27:37 +0000

    Caching FileWriter

--- src/com/java/zinc/classes/RepoIndexWriter.java
+++ src/com/java/zinc/classes/RepoIndexWriter.java
@@ -16,6 +16,8 @@ public class RepoIndexWriter {
     private final RepoIndex mRepoIndex;
     private final Gson mGson;
 
+    private FileWriter mFileWriter;
+
     public RepoIndexWriter(final File root, final Gson gson) {
         mGson = gson;
         mIndexFile = new File(root, REPO_INDEX_FILE);
@@ -25,11 +27,7 @@ public class RepoIndexWriter {
 
     private RepoIndex initializeIndex() {
         try {
-            final FileReader fileReader = new FileReader(mIndexFile);
-            final RepoIndex result = mGson.fromJson(fileReader, RepoIndex.class);
-            fileReader.close();
-
-            return result;
+            return mGson.fromJson(new FileReader(mIndexFile), RepoIndex.class);
         } catch (FileNotFoundException fnfe) {
             try {
                 mIndexFile.createNewFile();
@@ -38,19 +36,30 @@ public class RepoIndexWriter {
             } catch (IOException ioe) {
                 throw new ZincRuntimeException("Error creating index file", ioe);
             }
-        } catch (IOException e) {
-            throw new ZincRuntimeException("Error reading index file", e);
         }
     }
 
-    public void saveIndex() {
-        try {
-            final FileWriter fileWriter = new FileWriter(mIndexFile);
-            mGson.toJson(mRepoIndex, fileWriter);
-            fileWriter.close();
+    private FileWriter getFileWriter() {
+        if (mFileWriter == null) {
+            try {
+                mFileWriter = new FileWriter(mIndexFile);
+            } catch (IOException e) {
+                throw new ZincRuntimeException("Cannot write to index file", e);
+            }
+        }
 
+        return mFileWriter;
+    }
+
+    public void saveIndex() {
+        final FileWriter fileWriter = getFileWriter();
+
+        mGson.toJson(mRepoIndex, fileWriter);
+
+        try {
+            fileWriter.flush();
         } catch (IOException e) {
-            throw new ZincRuntimeException("Error saving index", e);
+            throw new ZincRuntimeException("Error writing to index file", e);
         }
     }
 
