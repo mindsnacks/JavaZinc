846e56a18ecd58dc8220efa2916c0a4af8dbd548
NachoSoto <ignaciosoto90@gmail.com>
2013-09-04 21:23:40 +0000

    ZincDownloadFileJob

--- src/com/java/zinc/classes/jobs/ZincDownloadFileJob.java
+++ src/com/java/zinc/classes/jobs/ZincDownloadFileJob.java
@@ -1,28 +1,44 @@
 package zinc.classes.jobs;
 
-import java.io.File;
+import java.io.*;
 import java.net.URL;
 
 /**
  * User: NachoSoto
  * Date: 9/4/13
  */
-public class ZincDownloadFileJob extends ZincDownloadJob<File> {
-    private final File mPath;
+public class ZincDownloadFileJob extends AbstractZincDownloadJob<File> {
+    private static final int BUFFER_SIZE = 1024;
 
-    public ZincDownloadFileJob(final ZincRequestExecutor requestExecutor, final URL url, final File path) {
+    private final File mFile;
+
+    public ZincDownloadFileJob(final ZincRequestExecutor requestExecutor, final URL url, final File root, final String filename) {
         super(requestExecutor, url, File.class);
-        mPath = path;
+        mFile = new File(root, filename);
     }
 
     @Override
-    public File call() throws DownloadFileError {
-        if (!mPath.exists()) {
-            if (!mPath.mkdir()) {
-                throw new DownloadFileError("Error creating folder: " + mPath.getAbsolutePath());
+    public File call() throws DownloadFileError, IOException {
+        final InputStreamReader inputStream = mRequestExecutor.get(mUrl);
+        FileWriter outputStream = null;
+
+        try {
+            outputStream = new FileWriter(mFile);
+
+            int read = 0;
+            char[] bytes = new char[BUFFER_SIZE];
+
+            while ((read = inputStream.read(bytes, 0, BUFFER_SIZE)) != -1) {
+                outputStream.write(bytes, 0, read);
+            }
+        } catch (IOException e) {
+            throw new DownloadFileError("Error writing to file '" + mFile.getAbsolutePath() + "'", e);
+        } finally {
+            if (outputStream != null) {
+                outputStream.close();
             }
         }
 
-        return null;
+        return mFile;
     }
 }
--- src/test/java/zinc/jobs/ZincDownloadFileJobTest.java
+++ src/test/java/zinc/jobs/ZincDownloadFileJobTest.java
@@ -5,12 +5,26 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.mockito.Mock;
+import utils.MockFactory;
 import utils.ZincBaseTest;
 import zinc.classes.jobs.ZincDownloadFileJob;
 import zinc.classes.jobs.ZincRequestExecutor;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * User: NachoSoto
@@ -23,20 +37,62 @@ public class ZincDownloadFileJobTest extends ZincBaseTest {
     @Rule
     public final TemporaryFolder rootFolder = new TemporaryFolder();
 
+    private final String mFilename;
+
     private final URL mUrl;
     private ZincDownloadFileJob mJob;
 
     public ZincDownloadFileJobTest() throws MalformedURLException {
         mUrl = new URL("http://mindsnacks.com");
+        mFilename = "file";
     }
 
     @Before
     public void setUp() throws Exception {
-        mJob = new ZincDownloadFileJob(mRequestExecutor, mUrl, rootFolder.getRoot());
+        mJob = new ZincDownloadFileJob(mRequestExecutor, mUrl, rootFolder.getRoot(), mFilename);
+    }
+
+    private File run() throws IOException {
+        return mJob.call();
     }
 
     @Test
-    public void testCreatesFolder() throws Exception {
+    public void createsFile() throws Exception {
+        setURLContents("Hello World");
 
+        final File file = run();
+
+        assertTrue(file.exists());
+    }
+
+    @Test
+    public void requestsURL() throws Exception {
+        setURLContents("Hello World");
+
+        run();
+
+        verify(mRequestExecutor).get(mUrl);
+    }
+
+    @Test
+    public void writesContentToFile() throws Exception {
+        final String contents = "Hello World";
+
+        setURLContents(contents);
+
+        final File file = run();
+
+        assertEquals(contents, readFile(file.getPath(), StandardCharsets.UTF_8));
+    }
+
+    private void setURLContents(final String contents) {
+        final InputStreamReader reader = MockFactory.inputStreamReaderWithString(contents);
+
+        when(mRequestExecutor.get(mUrl)).thenReturn(reader);
+    }
+
+    static String readFile(String path, Charset encoding) throws IOException {
+        final byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return encoding.decode(ByteBuffer.wrap(encoded)).toString();
     }
 }
