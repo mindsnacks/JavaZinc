45316cc9eac2564ec9327c375a3fceb578d2e39c
NachoSoto <ignaciosoto90@gmail.com>
2013-09-05 00:58:25 +0000

    ZincBundle

---
+++ src/com/java/zinc/classes/ZincBundle.java
@@ -0,0 +1,21 @@
+package zinc.classes;
+
+import java.io.File;
+
+/**
+ * User: NachoSoto
+ * Date: 9/4/13
+ */
+public class ZincBundle extends File {
+    final private String mBundleID;
+
+    public ZincBundle(final String root, final String bundleID) {
+        super(root, bundleID);
+
+        mBundleID = bundleID;
+    }
+
+    public String getBundleID() {
+        return mBundleID;
+    }
+}
