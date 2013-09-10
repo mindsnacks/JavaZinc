1ba8231981d4a08d0de042e166ac12ef66b00e9d
NachoSoto <ignaciosoto90@gmail.com>
2013-09-10 21:07:18 +0000

    ZincRepoFactory.DaemonThreadFactory now mimics DefaultThreadFactory

--- src/main/java/com/zinc/classes/ZincRepoFactory.java
+++ src/main/java/com/zinc/classes/ZincRepoFactory.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * User: NachoSoto
@@ -31,11 +32,32 @@ public final class ZincRepoFactory {
         return new ZincRepo(jobFactory, root.toURI(), indexWriter, flavorName);
     }
 
-    private class DaemonThreadFactory implements ThreadFactory {
+    // extracted from Executors.DefaultThreadFactory (which unfortunately is private)
+    private static class DaemonThreadFactory implements ThreadFactory {
+        private static final AtomicInteger poolNumber = new AtomicInteger(1);
+        private final ThreadGroup group;
+        private final AtomicInteger threadNumber = new AtomicInteger(1);
+        private final String namePrefix;
+
+        DaemonThreadFactory() {
+            final SecurityManager s = System.getSecurityManager();
+            group = (s != null) ? s.getThreadGroup() :
+                    Thread.currentThread().getThreadGroup();
+            namePrefix = "pool-" +
+                    poolNumber.getAndIncrement() +
+                    "-thread-";
+        }
+
         @Override
-        public Thread newThread(final Runnable r) {
-            final Thread t = new Thread(r);
+        public Thread newThread(Runnable r) {
+            Thread t = new Thread(group, r,
+                    namePrefix + threadNumber.getAndIncrement(),
+                    0);
             t.setDaemon(true);
+
+            if (t.getPriority() != Thread.NORM_PRIORITY)
+                t.setPriority(Thread.NORM_PRIORITY);
+
             return t;
         }
     }
