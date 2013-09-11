7ff219fd74e2116342d79a89759d8f850fe4bc0e
NachoSoto <ignaciosoto90@gmail.com>
2013-09-11 00:25:40 +0000

    Implemented and tested Gzip helper

--- src/main/java/com/zinc/classes/fileutils/GzipHelper.java
+++ src/main/java/com/zinc/classes/fileutils/GzipHelper.java
@@ -2,12 +2,36 @@ package com.zinc.classes.fileutils;
 
 import com.zinc.classes.data.ZincBundle;
 
+import java.io.*;
+import java.util.zip.GZIPInputStream;
+
 /**
  * User: NachoSoto
  * Date: 9/10/13
  */
 public class GzipHelper {
-    public void unzipFile(final ZincBundle bundle, final String filename, final String destination) {
+    public static final int BUFFER_SIZE = 8192;
 
+    public void unzipFile(final ZincBundle bundle, final String filename, final String destination) throws IOException {
+        final File input = new File(bundle, filename),
+                   output = new File(bundle, destination);
+
+        if (!input.exists()) {
+            throw new FileNotFoundException("File not found: " + input.getAbsolutePath());
+        }
+
+        final InputStream in = new BufferedInputStream(new GZIPInputStream(new FileInputStream(input)));;
+        final OutputStream dest = new BufferedOutputStream(new FileOutputStream(output));
+        try {
+            int count;
+            final byte data[] = new byte[BUFFER_SIZE];
+
+            while ((count = in.read(data)) != -1) {
+                dest.write(data, 0, count);
+            }
+        } finally {
+            dest.close();
+            in.close();
+        }
     }
 }
---
+++ src/test/java/com/zinc/classes/fileutils/GzipHelperTest.java
@@ -0,0 +1,68 @@
+package com.zinc.classes.fileutils;
+
+import com.zinc.classes.data.BundleID;
+import com.zinc.classes.data.ZincBundle;
+import com.zinc.utils.TestUtils;
+import com.zinc.utils.ZincBaseTest;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.*;
+import java.util.zip.GZIPOutputStream;
+
+import static com.zinc.utils.MockFactory.randomString;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * User: NachoSoto
+ * Date: 9/10/13
+ */
+public class GzipHelperTest extends ZincBaseTest {
+    private GzipHelper mHelper;
+    private ZincBundle mBundle;
+
+    @Rule public final TemporaryFolder rootFolder = new TemporaryFolder();
+
+    private final String mFilename = "file.gz";
+    private final String mDestination = "file.txt";
+    private final String mContents = randomString();
+
+    @Before
+    public void setUp() throws Exception {
+        mHelper = new GzipHelper();
+        mBundle = new ZincBundle(rootFolder.getRoot(), new BundleID("com.mindsnacks.catalog", "mBundle name"), 2);
+
+        createGzipFile(mContents, new File(mBundle, mFilename));
+    }
+
+    @Test
+    public void createsFile() throws Exception {
+        mHelper.unzipFile(mBundle, mFilename, mDestination);
+
+        assertTrue(new File(mBundle, mDestination).exists());
+    }
+
+    @Test
+    public void testUnzipFile() throws Exception {
+        mHelper.unzipFile(mBundle, mFilename, mDestination);
+
+        assertEquals(mContents, TestUtils.readFile(new File(mBundle, mDestination)));
+    }
+
+    private void createGzipFile(final String contents, final File file) throws IOException {
+       final FileOutputStream output = new FileOutputStream(file);
+        try {
+            final Writer writer = new OutputStreamWriter(new GZIPOutputStream(output));
+            try {
+                writer.write(contents);
+            } finally {
+                writer.close();
+            }
+        } finally {
+            output.close();
+        }
+    }
+}
