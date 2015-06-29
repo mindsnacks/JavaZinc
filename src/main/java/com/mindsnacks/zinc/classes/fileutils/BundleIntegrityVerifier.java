1c37d43286ce463a340651cb51ec055c20010316
Miguel Carranza <miguel@mindsnacks.com>
2015-06-29 14:19:17 -0700

    Cleaning it up

--- src/main/java/com/mindsnacks/zinc/classes/fileutils/BundleIntegrityVerifier.java
+++ src/main/java/com/mindsnacks/zinc/classes/fileutils/BundleIntegrityVerifier.java
@@ -3,28 +3,36 @@ package com.mindsnacks.zinc.classes.fileutils;
 import com.mindsnacks.zinc.classes.data.ZincManifest;
 import com.pegasus.data.accounts.PegasusAccountFieldValidator;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
  * Created by Miguel Carranza on 6/28/15.
  */
 public class BundleIntegrityVerifier {
+    private static final int BUFFER_SIZE = 8192;
+
     public static boolean isLocalBundleValid(final File localBundleFolder,
                                              final ZincManifest manifest,
                                              final String flavorName) {
         boolean isValid = true;
         final Map<String, ZincManifest.FileInfo> files = manifest.getFilesWithFlavor(flavorName);
+        final Iterator<String> it = files.keySet().iterator();
 
-        for (final Map.Entry<String, ZincManifest.FileInfo> entry : files.entrySet()) {
-            final String fileName = entry.getKey();
-            final ZincManifest.FileInfo fileInfo = entry.getValue();
-            final File localFile = new File(localBundleFolder, fileName);
+        while(it.hasNext() && isValid) {
+            final String fileName = it.next();
+            final ZincManifest.FileInfo fileInfo = files.get(fileName);
             final String expectedHash = fileInfo.getHash();
-            isValid = isValid && isLocalFileValid(localFile, expectedHash);
+            final File localFile = new File(localBundleFolder, fileName);
+
+            isValid &= isLocalFileValid(localFile, expectedHash);
         }
 
         return isValid;
@@ -37,11 +45,20 @@ public class BundleIntegrityVerifier {
 
         try {
             final ValidatingDigestInputStream digestStream = hashUtil.wrapInputStreamWithDigest(new FileInputStream(localFile));
+            final InputStream in = new BufferedInputStream(digestStream);
+            readInputStream(in);
+            in.close();
             digestStream.validate(expectedHash);
             isValid = true;
         } catch (FileNotFoundException e) {
-        } catch (ValidatingDigestInputStream.HashFailedException e) {}
+        } catch (ValidatingDigestInputStream.HashFailedException e) {
+        } catch (IOException e) {}
 
         return isValid;
+    }
+
+    private static void readInputStream(final InputStream stream) throws IOException {
+        final byte[] bytes = new byte[BUFFER_SIZE];
+        for (int read = 0; (read = stream.read(bytes, 0, BUFFER_SIZE)) !=-1;) {}
     }
 }
--- src/main/java/com/mindsnacks/zinc/classes/fileutils/ValidatingDigestOutputStream.java
+++ src/main/java/com/mindsnacks/zinc/classes/fileutils/ValidatingDigestOutputStream.java
@@ -17,18 +17,10 @@ public class ValidatingDigestOutputStream extends DigestOutputStream {
     }
 
     public void validate(String expectedHash) throws HashFailedException {
-        String hash = toHexString(getMessageDigest());
+        String hash = MessageDigestUtils.toHexString(getMessageDigest());
         if (!hash.equals(expectedHash)) {
             throw new HashFailedException("File hash (" + hash + ") does not match expected hash (" + expectedHash + ").");
         }
-    }
-
-    public String toHexString(MessageDigest digest) {
-        StringBuilder builder = new StringBuilder();
-        for (byte b : digest.digest()) {
-            builder.append(String.format("%02x", b));
-        }
-        return builder.toString();
     }
 
     public static final class HashFailedException extends ZincException {
